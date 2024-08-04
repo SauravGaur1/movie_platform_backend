73067a63b4408e75b9bb31802e7551d369428e9d
SauravGaur1 <sgaur70155@gmail.com>
2024-08-05 00:04:00 +0530

    refactor

--- src/services/jwt.js
+++ src/services/jwt.js
@@ -1,50 +1,21 @@
 const jwt = require("jsonwebtoken");
 const jwt_secret_key = require("../config/config.js").jsonWebToken.secretKey;
+const { isPlainObject } = require('../utils/validators.js');
 
 class JWT {
-  static createToken(payload) {
+  static async createToken(payload) {
     try {
-      const token = jwt.sign(payload, jwt_secret_key);
-      return token;
-    } catch (error) {
-      throw new Error("Error Creating Token: " + error);
-    }
+      if(!isPlainObject(payload)) throw new Error("Not a Valid Object to generate Token")
+      
+      return await jwt.sign(payload, jwt_secret_key);
+    } catch (error) {}
   }
 
   static async verifyToken(token) {
     try {
-      const extractInfo = jwt.verify(token, jwt_secret_key);
-      return extractInfo;
-    } catch (error) {
-      throw new Error("Error Verifying Token", error.message);
-    }
+      return await jwt.verify(token, jwt_secret_key);
+    } catch (error) {}
   }
 }
-
-/*
-    // To see if it is working
-function main() {
-  try {
-    const user = {
-      id: "101",
-      email: "vagishmlk@gmail.com",
-      role: "admin",
-    };
-
-    const token = JWT.createToken(user);
-    console.log("\nGenerated Token: ", token);
-
-    const decoded = JWT.verifyToken(token);
-    console.log("\nDecoded Token Data: ", decoded);
-  } catch (err) {
-    console.error("Error", err.message);
-  }
-
-  // await console.log(token);
-  // JWT.verifyToken(token);
-}
-
-main();
-*/
 
 module.exports = JWT;
