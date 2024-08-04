533e0ef793a9693cd9d6d03aac02e10b04fa5d72
Vagish <Vagishmlk@gmail.com>
2024-08-04 16:24:20 +0530

    working JWT

--- src/services/jwt.js
+++ src/services/jwt.js
@@ -1,58 +1,63 @@
 const jwt = require("jsonwebtoken");
-const jwt_secret_key=require("../config/config.js").jsonWebToken;
+const jwt_secret_key = require("../config/config.js").jsonWebToken.secretKey;
 
-console.log('Loaded JWT_SECRET:', jwt_secret_key); // This should log your secret key
-const secret_key = process.env.JWT_SECRET;
+class JWT {
+  static async createToken(payload) {
+    try {
+      const token = await new Promise((resolve, reject) => {
+        jwt.sign(payload, jwt_secret_key, function (err, token) {
+          if (err) {
+            reject(err);
+          }
 
- class JWT {
-  static async createToken(userDetail) {
-    const payload = {
-      email: userDetail.email,
-      role: userDetail.role,
-    };
-    console.log('Secret Key:', secret_key);
-
-    try{
-        const token= await new Promise((resolve,reject)=>{
-            jwt.sign(payload,secret_key,{ algorithm: "HS256" },function(err,token){
-                if(err) reject(err);
-                resolve(token);
-            });
+          resolve(token);
         });
-        return token;
-        
+      });
+      //   console.log("token created: " + token);
+      return token;
+    } catch (error) {
+      throw new Error("Error Creating Token" + error);
     }
-    catch(error){
-        throw new Error('Error Creating Token' + error);
-    }
-}
-
+  }
 
   static async verifyToken(token) {
-
-    try{
-
-        const decoded = await new Promise ((resolve,reject)=>{
-            jwt.verify(token,secret_key,function(err,decoded){
-                if(err){
-                    reject(err);
-                }
-                resolve(decoded);
-            })
+    try {
+      const extractInfo = await new Promise((resolve, reject) => {
+        jwt.verify(token, jwt_secret_key, function (err, decoded) {
+          if (err) {
+            reject(err);
+          }
+          resolve(decoded);
         });
-        return decoded;
-    }
-    catch(error){
-        throw new Error('Error Verifying Token',error.message);
+      });
+      return extractInfo;
+    } catch (error) {
+      throw new Error("Error Verifying Token", error.message);
     }
   }
 }
 
-const user={
-    email:"vagishmlk@gmail.com",
-    role:"admin",
-}
-JWT.createToken(user);
+async function main() {
+  try {
+    const user = {
+      id: "101",
+      email: "vagishmlk@gmail.com",
+      role: "admin",
 
+    };
+
+    const token = await JWT.createToken(user);
+    console.log("Created Token: ", token);
+
+    const decoded = await JWT.verifyToken(token);
+    console.log("Decoded Token Data: ", decoded);
+  } catch (err) {
+    console.error("Error", err.message);
+  }
+
+  // await console.log(token);
+  // JWT.verifyToken(token);
+}
+main();
 
 module.exports = JWT;
