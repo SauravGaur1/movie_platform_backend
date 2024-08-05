ee195a66b217871a37b5b920f97bf6605e7a5e5b
Kanishka-Ahuja <kanishkahuja.242@gmail.com>
2024-08-05 22:16:54 +0530

    model/movie

---
+++ src/api/v1/movie/model.js
@@ -0,0 +1,68 @@
+const { Sequelize, DataTypes, Model } = require('sequelize');
+const sequelize = require('../../../database/mariaConn')
+
+class Movie extends Model {}
+
+Movie.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    desc: {
+      type: DataTypes.TEXT,
+    },
+    thumbnail_portrait: {
+      type: DataTypes.JSON,
+    },
+    thumbnail_landscape: {
+      type: DataTypes.JSON,
+    },
+    lang: {
+      type: DataTypes.JSON,
+      allowNull: false,
+    },
+    lang: {
+      type: DataTypes.JSON,
+      allowNull: false,
+    },
+    trailer: {
+        type: DataTypes.TEXT
+    },
+    genre: {
+        type: DataTypes.JSON
+    },
+    rating: {
+        type: DataTypes.INTEGER
+    },
+    certification_type: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    duration: {
+        type: DataTypes.TIME,
+        allowNull: false
+    },
+    director: {
+        type: DataTypes.STRING
+    },
+    cast: {
+        type: DataTypes.JSON
+    },
+    release: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+  },
+);
+
+console.log(Movie === sequelize.models.Movie); 
