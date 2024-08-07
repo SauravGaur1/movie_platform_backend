aa74108626ca9f6da11206cd6f71aec848223582
SauravGaur1 <sgaur70155@gmail.com>
2024-08-07 23:04:27 +0530

    asyn package

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const config = require('./config/config.js');
 const app  = require('./app.js');
-require('./database/index.js');
+const {models} = require('./database/index.js');
 const {sequelize} = require('./database/database.js');
+const async = require("async");
 
 const enviornment = config.enviornment.active === "production"
         ? config.enviornment.production 
@@ -14,7 +15,22 @@ const {
 
 
 (async () => {
-    await sequelize.sync(/*{alter: true}*/);
+    try {
+        async.series([
+            async ()=> {
+                await models.State.sync({alter: true})
+            },
+            async () => {
+                await models.City.sync({alter: true})
+            }
+        ], (err, data) => {
+            console.log(err, data);
+        })
+    } catch(e) {
+        console.log(e);
+    }
+    
+    // await sequelize.sync(/*{alter: true}*/);
     console.log('✔️✔️✔️ Connected With Database');
     app.listen(PORT, HOST , () => {
         console.log(`✔️✔️✔️ server started @: http://${HOST}:${PORT}`);
