9910e3083a7e8ea6c406723b3ac997ae917af40a
Vagish <Vagishmlk@gmail.com>
2024-08-11 02:50:06 +0530

    added console.error

--- src/services/encryption.js
+++ src/services/encryption.js
@@ -1,27 +1,19 @@
-let {salt} = require('../config/config.js').encryption;
-const bcrypt = require('bcrypt');
+let { salt } = require("../config/config.js").encryption;
+const bcrypt = require("bcrypt");
 
 class Encryptor {
-   
-    static async hash (
-        value
-    ) {
+    static async hash(value) {
         const hashedValue = await bcrypt.hash(value, salt);
         return hashedValue;
     }
-
-    static async compareHash ({
-        orignalValue,
-        hashedValue
-    }) {
-        try{
-            return bcrypt.compare(orignalValue, hashedValue);
-        } catch {
+    static async compareHash(originalValue, hashedValue) {
+        try {
+            return await bcrypt.compare(originalValue, hashedValue);
+        } catch (error) {
+            console.error("Error comparing hash:", error);
             return false;
         }
     }
-
 }
-
 
 module.exports = Encryptor;
