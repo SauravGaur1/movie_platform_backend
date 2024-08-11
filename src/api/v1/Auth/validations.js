4a567aa09f3e93d94ef2543ff87cd2e7b5c2d08f
Vagish <Vagishmlk@gmail.com>
2024-08-12 01:06:35 +0530

    Joi Validations updated for email and mobile requirements

--- src/api/v1/Auth/validations.js
+++ src/api/v1/Auth/validations.js
@@ -1,7 +1,7 @@
 const Joi = require("joi");
 
 const common = {
-    email: Joi.string().required(),
+    email: Joi.string().email().required(),
     password: Joi.string().required(),
     role: Joi.number().valid(0, 1).required(),
 };
@@ -13,7 +13,17 @@ const loginSchema = Joi.object({
 const signupSchema = Joi.object({
     ...common,
     name: Joi.string().required(),
-    mobile: Joi.number().required(),
+    mobile: Joi.number()
+        .integer()
+        .min(1000000000)
+        .max(9999999999)
+        .required()
+        .messages({
+            "number.base": "cannot contain decimal.",
+            "number.min": "Mobile number must be 10 digits long.",
+            "number.max": "Mobile number must be exactly 10 digits long.",
+            "any.required": "Mobile number is required.",
+        }),
 });
 
 module.exports = {
