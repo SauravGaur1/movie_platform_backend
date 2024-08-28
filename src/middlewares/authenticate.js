71df1c3efbb614ae902b8c2a952029bad9b198bd
Vagish <Vagishmlk@gmail.com>
2024-08-28 23:35:26 +0530

    fixed token verification, checked and tested, if token is verified, add payload to req.User

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,17 +1,38 @@
-const { sendFailureResp } = require('../utils/response');
-const JWT = require('../services/jwt.js');
+const { sendFailureResp, sendSuccessResp } = require("../utils/response");
+const JWT = require("../services/jwt.js");
+const { customError } = require("../utils/error.js");
+const { sanitizeToken } = require("../utils/sanitize.js");
+const { isEmpty } = require("../utils/validators.js");
 
 module.exports = {
-  authenticate: () => async (req, res, next) => {
-    try {
-      const decoded = await JWT.verifyToken(req.body.token);
-      req.User = decoded;
-      next();
-    } catch (error) {
-      return sendFailureResp(res, {
-        status: 401,
-        data: error.message,
-      });
-    }
-  },
+    authenticate: () => async (req, res, next) => {
+        console.log("inside authenticate");
+        try {
+          //req.cookies.token because of app.use(cookieParser()) in app.js;
+            const token = req.cookies.token;
+            if (isEmpty(token)) {
+                throw new customError({
+                    message: "Token not Found!",
+                    statusCode: 404,
+                });
+            }
+            const decodedPayload = await JWT.verifyToken(token);
+            if (isEmpty(decodedPayload)) {
+                throw new customError({
+                    message: "Invalid or Expired Token!",
+                    statusCode: 401,
+                });
+            }
+            req.User = decodedPayload;
+            next();
+        } catch (error) {
+            return sendFailureResp(res, {
+                status: error.statusCode,
+                data: {
+                    message: error.message,
+                    ...error.payload,
+                },
+            });
+        }
+    },
 };
