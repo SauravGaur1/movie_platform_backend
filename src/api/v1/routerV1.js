c4e13787515651026abf5b3532a000d418d248f8
Vagish <Vagishmlk@gmail.com>
2024-08-28 15:54:35 +0530

    V1 router now includes movie router

--- src/api/v1/routerV1.js
+++ src/api/v1/routerV1.js
@@ -3,10 +3,12 @@ const router = Router();
 
 const userRouter = require('./User/router.js');
 const authRouter = require('./Auth/router.js');
+const movieRouter=require('./Movie/router.js');
 
 
 router.use('/user', userRouter);
 router.use('/auth', authRouter);
+router.use('/movie',movieRouter);
 
 
 module.exports = router;
