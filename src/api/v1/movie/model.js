6231348fe534a870cfea6ba195c6c2a960a3f8ad
Vagish <Vagishmlk@gmail.com>
2024-08-28 16:01:04 +0530

    updated model now has functions to find and create movie

--- src/api/v1/movie/model.js
+++ src/api/v1/movie/model.js
@@ -1,84 +1,142 @@
 const { DataTypes, Model } = require("sequelize");
 
 const { sequelize } = require("../../../database/database.js");
+const { customError } = require("../../../utils/error.js");
 
 class Movie extends Model {
-  static associate(models) {
-    Movie.belongsTo(models.Certification, {
-      foreignKey: 'certification_type',
-    });
+    static associate(models) {
+        Movie.belongsTo(models.Certification, {
+            foreignKey: "certification_type",
+        });
 
-    Movie.hasMany(models.Show, {
-      foreignKey: 'movie_id',
-    });
-  }
+        Movie.hasMany(models.Show, {
+            foreignKey: "movie_id",
+        });
+    }
+
+    static async findMovie(title) {
+        try {
+            const movieFound = await Movie.findOne({
+                where: {
+                    title: title,
+                },
+            });
+            if (!movieFound) {
+                return false;
+            }
+            return movieFound;
+        } catch (err) {
+            throw new customError({ message: err.message });
+        }
+    }
+    static async createMovie({
+        title,
+        desc,
+        lang,
+        trailer,
+        certification_type,
+        duration,
+        director,
+        cast,
+        release,
+        thumbnail_portrait,
+        thumbnail_landscape,
+        genre,
+        rating,
+    }) {
+        try {
+            const movie = await Movie.create({
+                title,
+                desc,
+                lang,
+                trailer,
+                certification_type,
+                duration,
+                director,
+                cast,
+                release,
+                thumbnail_portrait,
+                thumbnail_landscape,
+                genre,
+                rating,
+            });
+
+            console.log(movie);
+            return true;
+        } catch (err) {
+            throw new customError({
+                message: err.message,
+            });
+        }
+    }
 }
 
 Movie.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        desc: {
+            type: DataTypes.TEXT,
+        },
+        thumbnail_portrait: {
+            type: DataTypes.JSON,
+        },
+        thumbnail_landscape: {
+            type: DataTypes.JSON,
+        },
+        lang: {
+            type: DataTypes.JSON,
+            allowNull: false,
+        },
+        trailer: {
+            type: DataTypes.TEXT,
+        },
+        genre: {
+            type: DataTypes.JSON,
+        },
+        rating: {
+            type: DataTypes.INTEGER,
+        },
+        certification_type: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        duration: {
+            type: DataTypes.TIME,
+            allowNull: false,
+        },
+        director: {
+            type: DataTypes.STRING,
+        },
+        cast: {
+            type: DataTypes.JSON,
+        },
+        release: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW, // Sets the default value to the current timestamp
+            onUpdate: DataTypes.NOW, // Updates the timestamp on record update
+        },
     },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    desc: {
-      type: DataTypes.TEXT,
-    },
-    thumbnail_portrait: {
-      type: DataTypes.JSON,
-    },
-    thumbnail_landscape: {
-      type: DataTypes.JSON,
-    },
-    lang: {
-      type: DataTypes.JSON,
-      allowNull: false,
-    },
-    trailer: {
-      type: DataTypes.TEXT,
-    },
-    genre: {
-      type: DataTypes.JSON,
-    },
-    rating: {
-      type: DataTypes.INTEGER,
-    },
-    certification_type: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    duration: {
-      type: DataTypes.TIME,
-      allowNull: false,
-    },
-    director: {
-      type: DataTypes.STRING,
-    },
-    cast: {
-      type: DataTypes.JSON,
-    },
-    release: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },    createdAt: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-    },
-    updatedAt: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW, // Sets the default value to the current timestamp
-      onUpdate: DataTypes.NOW, // Updates the timestamp on record update
-    },
-  },
 
-  {
-    timestamps: false,
-    sequelize,
-  }
+    {
+        timestamps: false,
+        sequelize,
+    }
 );
 
 module.exports = Movie;
