6ae00900e202536cd8cd49166fb1f580213c72ef
Vagish <Vagishmlk@gmail.com>
2024-08-30 17:34:42 +0530

    updated router checks to clear through middlewares first

--- src/api/v1/movie/router.js
+++ src/api/v1/movie/router.js
@@ -1,9 +1,13 @@
 const { Router } = require("express");
 const router = Router();
+
 const { authenticate } = require("../../../middlewares/authenticate.js");
+const {validate}=require("../../../middlewares/joivalidators.js");
+
 const { add,files } = require("./controller.js");
+const {movieAddSchema}=require("./validations.js");
 
 router.get("/files", [authenticate(),files]);
-router.post('/add',[authenticate(),add]);
+router.post('/add',[authenticate(),validate(movieAddSchema),add]);
 
 module.exports = router;
