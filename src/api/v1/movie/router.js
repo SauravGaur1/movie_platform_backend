5d24fba7427eebec7e47f3723e6cf460954f7449
Vagish <Vagishmlk@gmail.com>
2024-08-30 14:16:33 +0530

    authenticate request before hand

--- src/api/v1/movie/router.js
+++ src/api/v1/movie/router.js
@@ -3,7 +3,7 @@ const router = Router();
 const { authenticate } = require("../../../middlewares/authenticate.js");
 const { add,files } = require("./controller.js");
 
-router.get("/files", [files]);
-router.post("/add",[authenticate(),add]);
+router.get("/files", [authenticate(),files]);
+router.post('/add',[authenticate(),add]);
 
 module.exports = router;
