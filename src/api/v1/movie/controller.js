653ab89cdb4cdbe1205938122cc0d9c11f418044
Vagish <Vagishmlk@gmail.com>
2024-08-30 14:15:59 +0530

    added try catch

--- src/api/v1/movie/controller.js
+++ src/api/v1/movie/controller.js
@@ -55,55 +55,65 @@ module.exports = {
         }
     },
     files: async (req, res) => {
-        const movieNumber = 101;
-        const fileCategory = "thumbnail";
+        try {
+            const movieNumber = 101;
+            const fileCategory = "thumbnail";
 
-        const prefix = `movie/${movieNumber}/${fileCategory}`;
+            const prefix = `movie/${movieNumber}/${fileCategory}`;
 
-        const files = await listFiles(prefix);
-        if (!files || files.length == 0) {
-            throw new customError({
-                message: "No files found in specifies Directory",
-                statusCode: 404,
+            const files = await listFiles(prefix);
+            
+            if (!files || files.length == 0) {
+                throw new customError({
+                    message: "No files found in specifies Directory",
+                    statusCode: 404,
+                });
+            }
+            // Set response headers
+            // res.setHeader("Content-Type", "application/octet-stream");
+            // res.setHeader("Transfer-Encoding", "chunked");
+            res.setHeader(
+                "Content-Type",
+                "multipart/x-mixed-replace; boundary=--boundary"
+            );
+
+            // Stream each file one by one
+            for (const key of files) {
+                res.write(`--file-boundary\n`);
+                res.write(
+                    `Content-Disposition: attachment; filename=${encodeURIComponent(
+                        key.split("/").pop()
+                    )}\n`
+                );
+                res.write(`Content-Type: application/octet-stream\n\n`);
+
+                const fileStream = await downloadFile(key);
+                fileStream.pipe(res, { end: false });
+                await new Promise((resolve) => fileStream.on("end", resolve));
+
+                res.write(`\n--file-boundary--\n`);
+            }
+
+            res.end();
+
+            // const key = files[0]; // You can change this logic to download all files
+            // const s3Stream = await downloadFile(key);
+            // console.log(s3Stream);
+
+            // Set headers
+            // res.setHeader('Content-Disposition', `attachment; filename="${encodeURIComponent(key.split('/').pop())}"`);
+            // res.setHeader('Content-Type', 'application/octet-stream');
+
+            // Pipe the S3 stream to the response
+            // s3Stream.pipe(res);
+        } catch (err) {
+            return sendFailureResp(res, {
+                status: err.statusCode,
+                data: {
+                    message: err.message,
+                    ...err.payload,
+                },
             });
         }
-        // Set response headers
-        // res.setHeader("Content-Type", "application/octet-stream");
-        // res.setHeader("Transfer-Encoding", "chunked");
-        res.setHeader(
-            "Content-Type",
-            "multipart/x-mixed-replace; boundary=--boundary"
-        );
-
-        // Stream each file one by one
-        for (const key of files) {
-            res.write(`--file-boundary\n`);
-            res.write(
-                `Content-Disposition: attachment; filename=${encodeURIComponent(
-                    key.split("/").pop()
-                )}\n`
-            );
-            res.write(`Content-Type: application/octet-stream\n\n`);
-
-            const fileStream = await downloadFile(key);
-            fileStream.pipe(res, { end: false });
-            await new Promise((resolve) => fileStream.on("end", resolve));
-
-            res.write(`\n--file-boundary--\n`);
-        }
-        console.log("after for loop");
-
-        res.end();
-
-        // const key = files[0]; // You can change this logic to download all files
-        // const s3Stream = await downloadFile(key);
-        // console.log(s3Stream);
-
-        // Set headers
-        // res.setHeader('Content-Disposition', `attachment; filename="${encodeURIComponent(key.split('/').pop())}"`);
-        // res.setHeader('Content-Type', 'application/octet-stream');
-
-        // Pipe the S3 stream to the response
-        // s3Stream.pipe(res);
     },
 };
