a834474db8ad32cd2c4468046b99f610f0b24d32
Vagish <Vagishmlk@gmail.com>
2024-09-02 18:31:52 +0530

    updated middleware as per kanishka's

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -5,33 +5,42 @@ const { sanitizeToken } = require("../utils/sanitize.js");
 const { isEmpty } = require("../utils/validators.js");
 
 module.exports = {
-    authenticate: () => async (req, res, next) => {
-        try {
-          //req.cookies.token because of app.use(cookieParser()) in app.js;
-            const token = req.cookies.token;
-            if (isEmpty(token)) {
-                throw new customError({
-                    message: "Token not Found!",
-                    statusCode: 404,
+    authenticate:
+        (role = 0) =>
+        async (req, res, next) => {
+            try {
+                //req.cookies.token because of app.use(cookieParser()) in app.js;
+                const token = req.cookies.token;
+
+                if (isEmpty(token)) {
+                    throw new customError({
+                        message: "Token not Found!",
+                        statusCode: 404,
+                    });
+                }
+                const decoded = await JWT.verifyToken(token);
+                if (isEmpty(decoded)) {
+                    throw new customError({
+                        message: "Invalid or Expired Token!",
+                        statusCode: 401,
+                    });
+                }
+                if (decoded.role != role) {
+                    throw new customError({
+                        message: "Invalid Role!",
+                        statusCode: 401,
+                    });
+                }
+                req.User = decoded;
+                next();
+            } catch (error) {
+                return sendFailureResp(res, {
+                    status: error.statusCode,
+                    data: {
+                        message: error.message,
+                        ...error.payload,
+                    },
                 });
             }
-            const decodedPayload = await JWT.verifyToken(token);
-            if (isEmpty(decodedPayload)) {
-                throw new customError({
-                    message: "Invalid or Expired Token!",
-                    statusCode: 401,
-                });
-            }
-            req.User = decodedPayload;
-            next();
-        } catch (error) {
-            return sendFailureResp(res, {
-                status: error.statusCode,
-                data: {
-                    message: error.message,
-                    ...error.payload,
-                },
-            });
-        }
-    },
+        },
 };
