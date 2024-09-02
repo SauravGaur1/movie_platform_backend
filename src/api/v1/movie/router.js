95c34990d2ac432a34b2bbfa1071b2f599300c6d
Vagish <Vagishmlk@gmail.com>
2024-09-02 17:37:50 +0530

    router update for adding files

--- src/api/v1/movie/router.js
+++ src/api/v1/movie/router.js
@@ -4,10 +4,11 @@ const router = Router();
 const { authenticate } = require("../../../middlewares/authenticate.js");
 const {validate}=require("../../../middlewares/joivalidators.js");
 
-const { add,files } = require("./controller.js");
+const { addMovie,downloadFiles,addFiles } = require("./controller.js");
 const {movieAddSchema}=require("./validations.js");
 
-router.get("/files", [authenticate(),files]);
-router.post('/add',[authenticate(),validate(movieAddSchema),add]);
+router.get("/files", [authenticate(),downloadFiles]);
+router.post('/addMovie',[authenticate(),validate(movieAddSchema),addMovie]);
+router.post('/addFiles',[authenticate(),addFiles]);
 
 module.exports = router;
