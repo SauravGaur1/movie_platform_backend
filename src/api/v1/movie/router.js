57d13d85e286504516f01704d131a09e78fc89d7
Vagish <Vagishmlk@gmail.com>
2024-09-02 18:34:22 +0530

    updated authentication to check for admin

--- src/api/v1/movie/router.js
+++ src/api/v1/movie/router.js
@@ -7,8 +7,8 @@ const {validate}=require("../../../middlewares/joivalidators.js");
 const { addMovie,downloadFiles,addFiles } = require("./controller.js");
 const {movieAddSchema}=require("./validations.js");
 
-router.get("/files", [authenticate(),downloadFiles]);
-router.post('/addMovie',[authenticate(),validate(movieAddSchema),addMovie]);
-router.post('/addFiles',[authenticate(),addFiles]);
+router.get("/files", [authenticate(1),downloadFiles]);
+router.post('/addMovie',[authenticate(1),validate(movieAddSchema),addMovie]);
+router.post('/addFiles',[authenticate(1),addFiles]);
 
 module.exports = router;
