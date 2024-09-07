74dc17b3345dd636c0fb44ba9e14d0741d380520
Vagish <Vagishmlk@gmail.com>
2024-09-07 21:43:11 +0530

    problematic multer working

--- src/services/aws_s3.js
+++ src/services/aws_s3.js
@@ -1,6 +1,8 @@
 const AWS = require("aws-sdk");
 const fs = require("fs");
 const path = require("path");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
 const { customError } = require("../utils/error");
 
 const awsConfig = require("../config/config").aws;
@@ -14,25 +16,74 @@ const s3 = new AWS.S3({
 const BUCKET_NAME = awsConfig.bucketName;
 
 class S3Service {
-    // Static method to upload a file to S3
-    static async uploadFile(filePath, key, contentType) {
-        // Read the file content from the filePath
-        if (!fs.existsSync(filePath)) {
-            throw new customError("File not found", 400);
-        }
-        const fileContent = fs.readFileSync(filePath);
-        const params = {
-            Bucket: BUCKET_NAME,
-            Key: key, // File name you want to save as in S3
-            Body: fileContent,
-            ContentType: contentType,
-        };
+    static  uploadFile() {
+        return multer({
+            storage: multerS3({
+                s3: s3,
+                bucket: BUCKET_NAME,
+                contentType: multerS3.AUTO_CONTENT_TYPE,
 
+                key: function (req, file, cb) {
+                    try {
+                        console.log("inside key func")
+                        const movieId = req.body.movieId;
+                        const fileCategory = req.body.fileCategory;
+                        if (!movieId || !fileCategory) {
+                            throw new customError('Missing movieId or fileCategory in request body.');
+                        }
+                        console.log(file);
+                        const fileName = `movie/${movieId}/${fileCategory}/${fileCategory}1${path.extname(
+                            file.originalname
+                        )}`;
+                        cb(null, fileName);
+                        console.log("after cb");
+                    } catch (error) {
+                        new customError(
+                            `Failed to set file name: ${error.message}`,
+                            500
+                        );
+                    }
+                },
+            }),
+        })
+    }
+    // Static method to upload a file to S3
+    // static async uploadFile(filePath, key, contentType) {
+    //     // Read the file content from the filePath
+    //     if (!fs.existsSync(filePath)) {
+    //         throw new customError("File not found", 400);
+    //     }
+    //     const fileContent = fs.readFileSync(filePath);
+    //     const params = {
+    //         Bucket: BUCKET_NAME,
+    //         Key: key, // File name you want to save as in S3
+    //         Body: fileContent,
+    //         ContentType: contentType,
+    //     };
+
+    //     try {
+    //         const data = await s3.upload(params).promise();
+    //         return data.Location; // Return the URL of the uploaded file
+    //     } catch (error) {
+    //         throw new customError(`Failed to upload file:`, 500, error);
+    //     }
+    // }
+
+    static async uploadFileStream(stream, key, contentType) {
         try {
+            const params = {
+                Bucket: BUCKET_NAME,
+                Key: key, // File name you want to save as in S3
+                Body: fileContent,
+                ContentType: contentType,
+            };
             const data = await s3.upload(params).promise();
-            return data.Location; // Return the URL of the uploaded file
         } catch (error) {
-            throw new customError(`Failed to upload file:`, 500, error);
+            throw new customError(
+                `failed to upload Stream ${error.message}`,
+                500,
+                error
+            );
         }
     }
     // Static method to download a file from S3
@@ -43,7 +94,7 @@ class S3Service {
         };
 
         try {
-            const data =  s3.getObject(params).createReadStream();
+            const data = s3.getObject(params).createReadStream();
             return data; // Return the file data
         } catch (error) {
             throw new customError(`Failed to download file:`, 500, error);
@@ -88,7 +139,6 @@ module.exports = {
     downloadFile: S3Service.downloadFile,
     listFiles: S3Service.listFiles,
 };
-
 
 //----------------To Download a File----------------
 // (async () => {
